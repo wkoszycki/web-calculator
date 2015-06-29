5511ea37e4dbf91a2cd3608527a71e1670d57998
Wojciech Koszycki <wojciech.koszycki@coi.gov.pl>
2015-06-29 14:14:34 +0200

    Log info about integral

--- src/main/java/com/wkoszycki/calculator/integral/AsynchronousIntegralService.java
+++ src/main/java/com/wkoszycki/calculator/integral/AsynchronousIntegralService.java
@@ -7,49 +7,54 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import lombok.extern.slf4j.Slf4j;
+import java.util.logging.Level;
 
-@Slf4j
+import lombok.extern.java.Log;
+
+@Log
 public class AsynchronousIntegralService implements IntegralService {
 
-    @Override
-    public double calculateEtoX(DefiniteIntegralParameters definiteIntegralParameters) {
-        double result = 0;
-        try {
-            final List<Future<Double>> futureResults
-                = calculateEtoX((AsynchronousIntegralParameters) definiteIntegralParameters);
-            for (Future<Double> subIntegralFuture : futureResults) {
-                result += subIntegralFuture.get();
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            throw new IllegalStateException("Error while performing asynchronous tasks");
-        }
-        return result;
+  @Override
+  public double calculateEtoX(DefiniteIntegralParameters definiteIntegralParameters) {
+    double result = 0;
+    try {
+      final List<Future<Double>> futureResults
+          = calculateEtoX((AsynchronousIntegralParameters) definiteIntegralParameters);
+      for (Future<Double> subIntegralFuture : futureResults) {
+        result += subIntegralFuture.get();
+      }
+    } catch (InterruptedException | ExecutionException e) {
+      throw new IllegalStateException("Error while performing asynchronous tasks");
     }
+    return result;
+  }
 
-    public List<Future<Double>> calculateEtoX(AsynchronousIntegralParameters asynchronousIntegralParameters)
-        throws InterruptedException, ExecutionException {
+  public List<Future<Double>> calculateEtoX(
+      AsynchronousIntegralParameters asynchronousIntegralParameters)
+      throws InterruptedException, ExecutionException {
 
-        final Set<SubIntegral> subIntegrals = prepareSubIntegrals(asynchronousIntegralParameters);
+    final Set<SubIntegral> subIntegrals = prepareSubIntegrals(asynchronousIntegralParameters);
 
-        int numberOfThreads = defineNumberOfThreads(asynchronousIntegralParameters);
-        ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
-        return executor.invokeAll(subIntegrals);
+    int numberOfThreads = defineNumberOfThreads(asynchronousIntegralParameters);
+    ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
+    return executor.invokeAll(subIntegrals);
+  }
+
+  private int defineNumberOfThreads(AsynchronousIntegralParameters asynchronousIntegralParameters) {
+    return asynchronousIntegralParameters.numberOfThreads == 0 ? 1
+                                                               : asynchronousIntegralParameters.numberOfThreads;
+  }
+
+  public Set<SubIntegral> prepareSubIntegrals(
+      DefiniteIntegralParameters definiteIntegralParameters) {
+    HashSet<SubIntegral> subIntegrals = new HashSet<>();
+    log.log(Level.INFO, "Splitting integral from {0} to {1} for sub-tasks",
+            new Object[]{definiteIntegralParameters.startRange,
+                         definiteIntegralParameters.endRange});
+    for (long i = definiteIntegralParameters.startRange;
+         i < definiteIntegralParameters.endRange; i++) {
+      subIntegrals.add(new SubIntegral(i));
     }
-
-    private int defineNumberOfThreads(AsynchronousIntegralParameters asynchronousIntegralParameters) {
-        return asynchronousIntegralParameters.numberOfThreads == 0 ? 1
-            : asynchronousIntegralParameters.numberOfThreads;
-    }
-
-    public Set<SubIntegral> prepareSubIntegrals(
-        DefiniteIntegralParameters definiteIntegralParameters) {
-        HashSet<SubIntegral> subIntegrals = new HashSet<>();
-
-        for (long i = definiteIntegralParameters.startRange;
-            i < definiteIntegralParameters.endRange; i++) {
-            subIntegrals.add(new SubIntegral(i));
-        }
-        return subIntegrals;
-    }
+    return subIntegrals;
+  }
 }
